247ccb97099a91fd29dbe218bc22b24599f45e44
Peter Lu <plu@rodanandfields.com>
2015-11-12 16:19:35 -0800

    mouseHoverSponsorDataAndClickContinue update

--- src/main/java/com/rf/pages/website/StoreFrontHomePage.java
+++ src/main/java/com/rf/pages/website/StoreFrontHomePage.java
@@ -3,6 +3,7 @@ package com.rf.pages.website;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
@@ -190,8 +191,10 @@ public class StoreFrontHomePage extends RFWebsiteBasePage {
 	}
 
 	public void mouseHoverSponsorDataAndClickContinue() throws InterruptedException{
-		actions =  new Actions(RFWebsiteDriver.driver);
-		actions.moveToElement(driver.findElement(By.xpath("//div[@class='sponsorDataDiv']"))).click(driver.findElement(By.cssSelector("input[value='Select & Continue']"))).build().perform();
+		/*actions =  new Actions(RFWebsiteDriver.driver);
+		actions.moveToElement(driver.findElement(By.xpath("//div[@class='sponsorDataDiv']"))).click(driver.findElement(By.cssSelector("input[value='Select & Continue']"))).build().perform();*/
+		JavascriptExecutor js = (JavascriptExecutor)(RFWebsiteDriver.driver);
+		js.executeScript("arguments[0].click();", driver.findElement(By.xpath("//*[@id='search-results']/div[1]/div[1]//input[@value='Select & Continue']")));
 		logger.info("First result of sponsor has been clicked");
 		driver.waitForLoadingImageToDisappear();
 		driver.waitForPageLoad();

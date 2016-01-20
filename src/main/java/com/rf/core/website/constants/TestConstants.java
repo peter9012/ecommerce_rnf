31c28fd251b718449601d7640eeab33338a0b16e
Peter Lu <plu@rodanandfields.com>
2016-01-20 11:44:58 -0800

    PC_FIRST_NAME = "PC"; RC_FIRST_NAME = "RCUser";

--- src/main/java/com/rf/core/website/constants/TestConstants.java
+++ src/main/java/com/rf/core/website/constants/TestConstants.java
@@ -418,6 +418,8 @@ public class TestConstants {
 	public static final String CRM_LOGIN_USERNAME = "crmqa@rodanandfields.com.tst3";
 	public static final String CRM_LOGIN_PASSWORD = "test1234";
 	public static final String CRM_INVALID_LOGIN_USERNAME = "crmqaguest@rodanandfields";
+	public static final String PC_FIRST_NAME = "PC";
+	public static final String RC_FIRST_NAME = "RCUser";
 
 }
 

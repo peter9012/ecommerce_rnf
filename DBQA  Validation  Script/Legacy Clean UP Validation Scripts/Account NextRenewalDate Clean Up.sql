b42f8897ebaf174b8d9459ea8840c1b008978cb0
NAND KHADKA <nkhadka@rodanandfields.com>
2016-02-29 14:28:58 -0800

    added Dev code in validation Scripts

--- DBQA  Validation  Script/Legacy Clean UP Validation Scripts/Account NextRenewalDate Clean Up.sql
+++ DBQA  Validation  Script/Legacy Clean UP Validation Scripts/Account NextRenewalDate Clean Up.sql
@@ -3,6 +3,70 @@ USE RFOperations
 GO 
 
 
+/* Developer's Code 
+
+
+------- RFO Cleanup Script for NextRenewalDate------
+USE RFOperations;
+GO 
+
+--DISABLE TRIGGER RFO_Accounts.[utAuditRFOAccountsAccountRF] ON RFO_Accounts.AccountRF
+
+
+SELECT  COUNT(*)
+FROM    RFOperations.RFO_Accounts.AccountRF a
+        JOIN RFOperations.RFO_Accounts.AccountBase ab ON ab.AccountID = a.AccountID
+        JOIN RFOperations.RFO_Accounts.AccountContacts ac ON ab.AccountID = ac.AccountId
+WHERE   NextRenewalDate IS NOT NULL
+        AND ab.CountryID = 236
+        AND FirstName NOT LIKE '%test%';
+		   --15 records in STG2 before Update
+		   --168360 records in STG2 after Update.
+
+		   
+DECLARE @Getdate DATETIME2 = GETDATE();
+SELECT  RF.AccountID ,
+        NextRenewalDate ,
+        CASE WHEN LastRenewalDate IS NOT NULL
+
+             THEN DATEADD(YEAR, 1, RF.LastRenewalDate)
+             WHEN EnrollmentDate >= DATEADD(YEAR, -1, CAST(GETDATE() AS DATE))
+             THEN DATEADD(YEAR, 1, EnrollmentDate)
+             ELSE NextRenewalDate
+        END AS New_NextRenewalDate ,
+        RF.ServerModifiedDate ,
+        @Getdate AS newServerModifiedDate
+INTO    RFOperations..Cleaup_AccountNextRenewalDate_1
+FROM    RFOperations.RFO_Accounts.AccountRF RF
+        JOIN RFOperations.RFO_Accounts.AccountBase ab ON ab.AccountID = RF.AccountID
+WHERE   ab.CountryID = 236
+        AND ab.AccountTypeID = 1; 
+
+		--298477 Counts before Update in STG2
+
+		   -- 0 Records 
+
+BEGIN TRAN;  
+DECLARE @Getdate DATETIME2 = GETDATE();	   
+UPDATE  RF
+SET     NextRenewalDate = New_NextRenewalDate ,
+        --RF.ServerModifiedDate = @Getdate ,
+        RF.ChangedByApplication = 'US-HybrisLaunch' ,
+        RF.ChangedByUser = 'GP-1790'
+FROM    RFOperations.RFO_Accounts.AccountRF RF
+        JOIN RFOperations..Cleaup_AccountNextRenewalDate_1 ab ON ab.AccountID = RF.AccountID;
+		   --WHERE ab.CountryID =236 AND ab.AccountTypeID = 1 
+
+
+-- COMMIT 
+
+--ENABLE TRIGGER RFO_Accounts.[utAuditRFOAccountsAccountRF] ON RFO_Accounts.AccountRF
+
+
+*/
+
+
+
 /* Checking if Any Accounts with NextRenewalDate is NULL after Update. */
 
 
@@ -65,6 +129,13 @@ GO
         ADD NextRenewalDate DATETIME;
 
 
+	/* Validation added Column in Staging Accounts table.*/
+
+	
+SELECT * FROM RFOperations.INFORMATION_SCHEMA.Columns 
+WHERE TABLE_NAME LIKE '%StagingAccounts' AND COLUMN_NAME='NextRenewalDate'
+
+
 
 
 		/* Modification within SP.*/
@@ -90,3 +161,24 @@ GO
 
 
 
+		 SELECT  COUNT(*) --Should be Null after update.
+	--SELECT ab.AccountID,ab.AccountTypeID,NextRenewalDate,LastRenewalDate,EnrollmentDate,ab.CountryID 
+    FROM    RFOperations.RFO_Accounts.AccountRF RF
+            JOIN RFOperations.RFO_Accounts.AccountBase ab ON ab.AccountID = RF.AccountID
+    WHERE   ab.CountryID = 236
+            AND ab.AccountTypeID = 1
+            AND ( ( LastRenewalDate IS NULL
+                    AND NextRenewalDate <> DATEADD(YEAR, 1, EnrollmentDate)
+                    AND EnrollmentDate >= DATEADD(YEAR, -1,
+                                                  CAST(GETDATE() AS DATE))
+                  )
+                  OR ( LastRenewalDate IS NOT NULL
+                       AND NextRenewalDate <> DATEADD(YEAR, 1,
+                                                      RF.LastRenewalDate)
+                     )
+                  OR ( LastRenewalDate IS   NULL
+                       AND NextRenewalDate IS NOT NULL 
+                       AND EnrollmentDate < DATEADD(YEAR, -1,
+                                                    CAST(GETDATE() AS DATE))
+                     )
+                );

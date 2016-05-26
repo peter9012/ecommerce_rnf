d4cfef92d368b826ee5c813362dbb54c8efb221d
Peter Lu <plu@rodanandfields.com>
2016-05-26 10:57:38 -0700

    FNSCoreWebsiteBaseTest

--- src/test/java/com/rf/test/website/RFNSCoreWebsiteBaseTest.java
+++ src/test/java/com/rf/test/website/RFNSCoreWebsiteBaseTest.java
@@ -60,11 +60,11 @@ public class RFNSCoreWebsiteBaseTest extends RFBaseTest {
 	public void beforeMethod(){
 		logger.info("In Before method..");
 		s_assert = new SoftAssert();
-		if(driver.getURL().contains("nsc4"))
+/*		if(driver.getURL().contains("nsc4"))
 			driver.get(driver.getURL());	
 		else{
 			driver.get(driver.getURL()+"/Home.aspx");	
-		}
+		}*/
 		logger.info("Out of Before method..");
 	}
 
@@ -89,6 +89,12 @@ public class RFNSCoreWebsiteBaseTest extends RFBaseTest {
 
 	@AfterMethod
 	public void tearDownAfterMethod(){
+		if(driver.getURL().contains("nsc4"))
+			driver.get(driver.getURL());	
+		else{
+			driver.get(driver.getURL()+"/Home.aspx");	
+		}
+		login("admin", "skin123!");
 
 	}
 

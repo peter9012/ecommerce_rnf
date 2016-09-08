88da3b92245003a36045427a83830261e43c8fdf
shubham mathur <shubham.mathur@gspann.com>
2016-09-08 14:15:15 +0530

    heirloom fixes

--- src/test/java/com/rf/test/website/storeFront/dsv/StoreFrontDSVTestsCleanProfiles.java
+++ src/test/java/com/rf/test/website/storeFront/dsv/StoreFrontDSVTestsCleanProfiles.java
@@ -1,6 +1,8 @@
 package com.rf.test.website.storeFront.dsv;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
 import com.rf.core.website.constants.TestConstants;
 import com.rf.pages.website.dsv.DSVStoreFrontBillingInfoPage;
 import com.rf.pages.website.dsv.DSVStoreFrontHomePage;
@@ -9,8 +11,8 @@ import com.rf.test.website.RFWebsiteBaseTest;
 public class StoreFrontDSVTestsCleanProfiles extends RFWebsiteBaseTest{
 	private DSVStoreFrontHomePage dsvStoreFrontHomePage;
 	private DSVStoreFrontBillingInfoPage dsvStoreFrontBillingInfoPage;
-	
-	
+
+
 	@Test
 	public void cleanAllBillingProfilesConsultant() throws Exception{
 		dsvStoreFrontHomePage = new DSVStoreFrontHomePage(driver);
@@ -20,9 +22,15 @@ public class StoreFrontDSVTestsCleanProfiles extends RFWebsiteBaseTest{
 		dsvStoreFrontHomePage.clickLoginBtn();
 		dsvStoreFrontHomePage.clickWelcomeDropDown();
 		dsvStoreFrontBillingInfoPage = dsvStoreFrontHomePage.clickBillingInfoLinkFromWelcomeDropDown();
-		dsvStoreFrontBillingInfoPage.cleanAllBillingProfiles();
+		int totalBillingProfiles = dsvStoreFrontBillingInfoPage.getTotalBillingProfiles();
+		for(int i=1;i<=totalBillingProfiles;i++){
+			if(dsvStoreFrontBillingInfoPage.areMoreBillingProfilesLeftForDeletion())
+				Assert.assertTrue(dsvStoreFrontBillingInfoPage.deleteBillingProfiles(),"'Your Billing profile has been removed' message has not displayed");
+			else
+				break;
+		}
 	}
-	
+
 	@Test
 	public void cleanAllBillingProfilesPC() throws Exception{
 		dsvStoreFrontHomePage = new DSVStoreFrontHomePage(driver);
@@ -32,9 +40,16 @@ public class StoreFrontDSVTestsCleanProfiles extends RFWebsiteBaseTest{
 		dsvStoreFrontHomePage.clickLoginBtn();
 		dsvStoreFrontHomePage.clickWelcomeDropDown();
 		dsvStoreFrontBillingInfoPage = dsvStoreFrontHomePage.clickBillingInfoLinkFromWelcomeDropDown();
-		dsvStoreFrontBillingInfoPage.cleanAllBillingProfiles();
+		int totalBillingProfiles = dsvStoreFrontBillingInfoPage.getTotalBillingProfiles();
+		for(int i=1;i<=totalBillingProfiles;i++){
+			if(dsvStoreFrontBillingInfoPage.areMoreBillingProfilesLeftForDeletion())
+				Assert.assertTrue(dsvStoreFrontBillingInfoPage.deleteBillingProfiles(),"'Your Billing profile has been removed' message has not displayed");
+			else
+				break;
+		}
+
 	}
-	
+
 	@Test
 	public void cleanAllBillingProfilesRC() throws Exception{
 		dsvStoreFrontHomePage = new DSVStoreFrontHomePage(driver);
@@ -44,6 +59,12 @@ public class StoreFrontDSVTestsCleanProfiles extends RFWebsiteBaseTest{
 		dsvStoreFrontHomePage.clickLoginBtn();
 		dsvStoreFrontHomePage.clickWelcomeDropDown();
 		dsvStoreFrontBillingInfoPage = dsvStoreFrontHomePage.clickBillingInfoLinkFromWelcomeDropDown();
-		dsvStoreFrontBillingInfoPage.cleanAllBillingProfiles();
+		int totalBillingProfiles = dsvStoreFrontBillingInfoPage.getTotalBillingProfiles();
+		for(int i=1;i<=totalBillingProfiles;i++){
+			if(dsvStoreFrontBillingInfoPage.areMoreBillingProfilesLeftForDeletion())
+				Assert.assertTrue(dsvStoreFrontBillingInfoPage.deleteBillingProfiles(),"'Your Billing profile has been removed' message has not displayed");
+			else
+				break;
+		}
 	}
 }

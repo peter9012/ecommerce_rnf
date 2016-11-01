7fda3c8abbfb027d173a1ba76d57561e2190de8d
shubham mathur <shubham.mathur@gspann.com>
2016-11-01 12:30:33 +0530

    added the code to create the logs folder if not present

--- src/test/java/com/rf/test/base/RFBaseTest.java
+++ src/test/java/com/rf/test/base/RFBaseTest.java
@@ -54,13 +54,6 @@ public class RFBaseTest{
 			propertyFile.loadProps(envproperties);
 			logger.debug("Environment properties recieved and preparing the environment for "
 					+ envproperties); 
-//			logger.info("EXECUTION ENVIRONMENT ------ "+propertyFile.getProperty("browser"));
-//			logger.info("EXECUTION ENVIRONMENT ------ "+System.getProperty("browser"));
-//			logger.info("EXECUTION ENVIRONMENT ------ "+System.getProperty("baseURL"));
-//			logger.info("EXECUTION ENVIRONMENT ------ "+System.getProperty("country"));
-//			logger.info("EXECUTION ENVIRONMENT ------ "+System.getProperty("env"));
-//			logger.info("EXECUTION ENVIRONMENT ------ "+System.getProperty("dbIP"));
-
 		} else {
 			propertyFile.loadProps(defaultProps);
 			logger.info("Environment properties are not provided by the user ... loading the default properties");
@@ -91,6 +84,12 @@ public class RFBaseTest{
 				+ "\\test-output");
 		if (!fDir2.exists()) {
 			fDir2.mkdirs();
+		}
+		
+		File logDir = new File(System.getProperty("user.dir")
+				+ "\\logs");
+		if (!logDir.exists()) {
+			logDir.mkdirs();
 		}
 		
 		propertyFile.setProperty("atu.reports.dir",pathOfAtuReports);

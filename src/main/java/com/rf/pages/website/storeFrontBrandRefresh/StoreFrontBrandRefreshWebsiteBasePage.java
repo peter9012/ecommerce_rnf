88c489774601f3b6ef17409e7a13ea60487536c9
shubham mathur <shubham.mathur@gspann.com>
2016-11-09 20:57:49 +0530

    updated hybris reg tests

--- src/main/java/com/rf/pages/website/storeFrontBrandRefresh/StoreFrontBrandRefreshWebsiteBasePage.java
+++ src/main/java/com/rf/pages/website/storeFrontBrandRefresh/StoreFrontBrandRefreshWebsiteBasePage.java
@@ -398,6 +398,7 @@ public class StoreFrontBrandRefreshWebsiteBasePage extends RFBasePage{
 		alert.accept();
 		logger.info("Ok button of java Script popup is clicked.");
 		driver.waitForPageLoad();
+		driver.pauseExecutionFor(3000);
 	}
 
 	public String convertBizSiteToComSite(String pws){

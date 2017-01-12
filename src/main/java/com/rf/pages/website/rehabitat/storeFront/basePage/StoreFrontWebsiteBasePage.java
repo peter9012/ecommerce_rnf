e2df92d195891800be5211c02d08724ccb893f6d
shubham mathur <shubham.mathur@gspann.com>
2017-01-12 10:56:12 +0530

    modified scripts to take products from All Products instead of Essentials

--- src/main/java/com/rf/pages/website/rehabitat/storeFront/basePage/StoreFrontWebsiteBasePage.java
+++ src/main/java/com/rf/pages/website/rehabitat/storeFront/basePage/StoreFrontWebsiteBasePage.java
@@ -301,9 +301,9 @@ public class StoreFrontWebsiteBasePage extends RFBasePage{
 	 * @return
 	 */
 	public StoreFrontShopSkinCarePage clickAllProducts(){
-		//		mouseHoverOn(TestConstants.SHOP_SKINCARE);
-		//		driver.click(ALL_PRODUCTS_LOC);
-		clickCategoryLink("ESSENTIALS");
+		mouseHoverOn(TestConstants.SHOP_SKINCARE);
+		driver.click(ALL_PRODUCTS_LOC);
+		//clickCategoryLink("ESSENTIALS");
 		logger.info("clicked on 'All Products'");
 		driver.waitForPageLoad();
 		return new StoreFrontShopSkinCarePage(driver);

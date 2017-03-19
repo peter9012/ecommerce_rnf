8d7b5238b48f878953fa753ad09972586f971336
shubham mathur <shubham.mathur@gspann.com>
2017-03-19 14:09:08 +0530

    tests data updated

--- src/test/java/com/rf/test/website/rehabitat/storeFront/TopNavigationTest.java
+++ src/test/java/com/rf/test/website/rehabitat/storeFront/TopNavigationTest.java
@@ -151,7 +151,7 @@ public class TopNavigationTest extends StoreFrontWebsiteBaseTest{
 	 *  
 	 *     
 	 */
-	@Test(enabled=true)
+	@Test(enabled=false)
 	public void testAboutRFExecutiveTeam_76(){
 		String currentURL = null;
 		String teamMemberName = null;
